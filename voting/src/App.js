b78160b60c1585df181cba54ae5d146bc14fe6c7
AMALENDU MANOJ <220229@tkmce.ac.in>
2023-11-21 00:58:53 +0530

    frontend

--- voting/src/App.js
+++ voting/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count1,setcount1]=useState(0);
+  const [count2,setcount2]=useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="flex justify-center font-mono font-bold">
+        <h1>Voting For your candidate</h1>
+      </div>
+      <div className="flex justify-between gap-10">
+        <div className="flex flex-col gap-4 ">
+          <h2>candidate 1</h2>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">vote</button>
+          <p>Total:{count1}</p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h2>candidate 2</h2>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >vote</button>
+          <p>Total:{count2}</p>
+        </div>
+      </div>
+
     </div>
   );
 }
